a950a5dffda73ba92d5af97823827e1b1c34d48b
Павел <Pavel@mail.ru>
2021-06-21 21:10:08 +0400

    fix api server todo

--- src/components/todo/todo.tsx
+++ src/components/todo/todo.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import {Itodo} from "interface/itodo/itodo";
 import  "components/todo/todo.css"
+
 export interface iTodoProps {
     todo: Itodo
 }
 
 export const  Todo = ( props: iTodoProps) => {
+
     const  className = props.todo.active ? "todo__element todo__element_completed" : "todo__element todo__element_performed";
     return (
     <div className={className} >
         <p className="todo__text">
             {props.todo.text}
         </p>
+        <div className="todo__date todo__date_create">
+            Дата создания:
+        </div>
+        <div className="todo__date todo__date_execution">
+            Дата выполнения:
+        </div>
     </div>
     );
 }
